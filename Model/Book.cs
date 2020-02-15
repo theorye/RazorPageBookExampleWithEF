e9a8e91b4139541e997c97e03b578953537f9d13
Adrian <limitx3m@outlook.com>
2020-02-15 13:43:30 -0600

    initial commit

---
+++ Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]  // Means it can not be null
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+    }
+}
